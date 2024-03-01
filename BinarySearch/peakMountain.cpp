f37b6c348022f915878ee7bbfc8cb5d8858e804a
Deva <devaydv18@gmail.com>
2024-03-01 20:39:23 +0530

    peakMountain

---
+++ BinarySearch/peakMountain.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+// int main(){
+//     int s = 0;
+//     int e = 0;
+//     int mid = s + (e-s)/2;
+//     while(s<e){
+//         if(arr[mid]<arr[mid+1]){
+//             s = mid +1;
+//         }else{
+//             e = mid
+//         }
+//         mid = s+(e-s)/2;
+//     }
+//     return s;
+// }
+
+
+// leetcode problem 

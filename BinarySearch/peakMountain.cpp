1f5a6d2fcc67b00c6c71c64cc233584f114fd087
Deva <devaydv18@gmail.com>
2024-03-03 18:37:28 +0530

    sorting algo

--- BinarySearch/peakMountain.cpp
+++ BinarySearch/peakMountain.cpp
@@ -18,3 +18,56 @@ using namespace std;
 
 
 // leetcode problem 
+
+
+#include <iostream>
+using namespace std;
+
+int firstOcc(int arr[], int n, int key) {
+    int s = 0;
+    int e = n - 1;
+    int mid;
+    int ans = -1; // Initialize ans to -1, to indicate that key is not found yet.
+
+    while (s <= e) {
+        mid = s + (e - s) / 2; // Better way to calculate mid to avoid integer overflow
+        if (arr[mid] == key) {
+            ans = mid;
+            e = mid - 1; // Update e to search for the first occurrence in the left half
+        }
+        if (key > arr[mid]) {
+            s = mid + 1;
+        } else {
+            e = mid - 1;
+        }
+    }
+    return ans;
+}
+
+int lastOcc(int arr[], int n, int key) {
+    int s = 0;
+    int e = n - 1;
+    int mid;
+    int ans = -1; // Initialize ans to -1, to indicate that key is not found yet.
+
+    while (s <= e) {
+        mid = s + (e - s) / 2; // Better way to calculate mid to avoid integer overflow
+        if (arr[mid] == key) {
+            ans = mid;
+            s = mid + 1; // Update s to search for the last occurrence in the right half
+        }
+        if (key > arr[mid]) {
+            s = mid + 1;
+        } else {
+            e = mid - 1;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    int even[7] = {1, 2, 3, 3, 3, 3, 5};
+    cout << "First Occurrence: " << firstOcc(even, 7, 3) << endl;
+    cout << "Last Occurrence: " << lastOcc(even, 7, 3) << endl;
+    return 0;
+}
---
+++ SelectionSort/question1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+
+/*
+
+
+{
+
+
+for(int i =0; i<n-1; i++){
+    int midIndex = i;
+
+for (int j= i+1; j<n; j++){
+    if(arr[j]<arr[minIndex])
+    minIndex = j;
+}
+swap(arr[minIndex], arr[i])
+
+}
+
+
+
+}
+
+*/

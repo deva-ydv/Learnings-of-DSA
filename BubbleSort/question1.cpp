540611174f46fe5bd738957e230c87d4d7116d6c
Deva <devaydv18@gmail.com>
2024-03-04 16:12:34 +0530

    bubbleSort & insertionSort

---
+++ BubbleSort/question1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+/*
+for (int i = 1; j < n; i++)
+{
+    for (int j = 0; j < n - 1; j++)
+    {
+        if (arr[j] > arr[j + 1])
+        {
+            swap(arr[j], arr[j + 1]);
+        }
+    }
+}
+*/
---
+++ InsertionSort/question1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+// for(int i =1; i<n; i++){
+//     int temp = arr[i];
+//     int j = i-1;
+//     for(j>=0; j++){
+//         if(arr[i] > temp){
+//             //shift
+//             arr[j+1] = arr[j];
+//         }else {
+//             //stop
+//             break;
+//         }
+
+//     }
+//     arr[j+1] = temp;
+// }

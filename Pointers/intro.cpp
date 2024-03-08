0dbfe3b39271f67fd60170c8ce2a0c7b0b1bfe7c
Deva <devaydv18@gmail.com>
2024-03-08 18:59:17 +0530

    2D arrays, pointers

---
+++ Pointers/intro.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int num = 6;
+    //     cout << num<<endl;
+    //     cout<< "Address of num "<< &num;
+    // // this address is in hexa-decimal format
+    // // Pointers stores the address data
+
+    // int *ptr = &num;
+    // cout << "Address is "<< ptr<<endl;
+    // cout << "value is "<< *ptr;
+
+
+
+    // int *p = 0; // null->segmentation fault
+
+
+
+
+    /*int i = 5;
+
+    int *q = &i;
+
+    cout << "Address is "<< q<<endl;
+    cout << "value is "<< *q<<endl;
+    int *p;
+    p= &i;
+
+    cout << "Address is "<< p<<endl;
+    */
+
+
+
+
+    /*
+    int i = 2;
+    int *p= &i;
+    (*p)++;
+
+    cout<<i;*/
+}
